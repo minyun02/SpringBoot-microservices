b334c4d90ca56175015e93e7fff19d18e89037e4
min <www5981@naver.com>
2022-02-12 19:11:44 +0900

    routeLocator inline으로 리팩토링

--- api-gateway/src/main/java/com/mins/microservices/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/mins/microservices/apigateway/ApiGatewayConfiguration.java
@@ -16,14 +16,12 @@ public class ApiGatewayConfiguration {
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
         //customize routes
-        Function<PredicateSpec, Buildable<Route>> routeFunction
-                = p -> p.path("/get")
+        return builder.routes()
+                .route(p -> p.path("/get")
                 .filters(f -> f
                         .addRequestHeader("MyHeader", "MyURI")
                         .addRequestParameter("Param","MyValue"))
-                .uri("http://httpbin.org:80");
-        return builder.routes()
-                .route(routeFunction)
+                .uri("http://httpbin.org:80"))
                 .build();
     }
 }
